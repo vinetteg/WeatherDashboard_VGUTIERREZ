86818957185529e1c7e51a894415368a9ea391dd
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-05-01 15:31:13 -0700

    Adding more to README.md and javascript

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # WeatherDashboard_VGUTIERREZ
-Week 6 Homework - Weather dashboard 
+
+## Summary
+
+Week 6 Homework - Weather dashboard
+
+In this project, I built a weather dashboard that displays the current weather of a city that is inputted in the form.
+
+This project uses html, css and javascript elements. Bootstrap is used in order to build the html format. Moment.js is used in order the help display time/date functions.
+
+## Weather Dash Mock Up
+
+Here is a mockup of the page:
+
+![portfoliomenu](images/mockupimage.png)
+
+Link to application:
+[https://vinetteg.github.io/WeatherDashboard_VGUTIERREZ/]
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var userFormEl = document.querySelector("#user-form");
 var cityNameEl = document.querySelector("#username");
 var citiesContainerEl = document.querySelector("#cities-container");
+var weatherContainerEl = document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
 var cityEl = document.querySelector("#results-city");
 var cityInputEl = document.querySelector("#results-city");
+
 //button function
 var citySearch = function (event) {
   event.preventDefault();
@@ -10,46 +13,98 @@ var citySearch = function (event) {
   var cityState = cityNameEl.value.trim();
 
   if (cityState) {
-    getUserRepos(cityState);
+    getWeather(cityState);
 
     citiesContainerEl.textContent = "";
     cityNameEl.textContent = "";
-  } else alert("Please enter a city to start your search");
-};
-
-var getWeather = function (weather) {
-  var apiWeather =
-    "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}";
-  fetch(apiWeather)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-};
-
-var displayWeather = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    cityInputEl.textContent = "Please check city name that was entered";
-    return;
+  } else {
+    alert("Please enter a city to start your search");
   }
-  cityInputEl.textContent = searchTerm;
 
-  for (var i = 0; i < repos.length; i++) {
-    var cityNameResult = repos[i].owner.login;
+  var saveSearch = function () {
+    localStorage.setItem("cities", JSON.stringify(cities));
+  };
+  saveSearch();
+  pastSearch(cityState);
+};
 
-    var weatherEl = document.createElement("a");
-    weatherEl.classList = "list-item";
-    weatherEl.setAttribute(cityNameResult);
+var getWeather = function (city) {
+  var apiWeather =
+    "https://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID=9fc0d146e8b9c3492bad6e84401335c1";
 
-    var titleEl = document.createElement("span");
-    titleEl.textContent = cityNameResult;
+  fetch(apiWeather).then(function (response) {
+    response.json().then(function (data) {
+      displayWeather(data, city);
+    });
+  });
+};
 
-    weatherEl.appendChild(titleEl);
+var displayWeather = function (weather, searchCity) {
+  weatherContainerEl.textContent = "";
+  citySearchInputEl.textContent = searchCity;
 
-    repoContainerEl.appendChild(titleEl);
+  //date
+  var date = document.createElement("span");
+  date.textContent = "(" + moment().format("MMM DD, YYYY") + ")";
+  citySearchInputEl.appendChild(date);
+
+  //temperature
+  var temperature = document.createElement("span");
+  temperature.textContent = "Temperature:" + weather.main.temp + "°F";
+  temperature.classList = "list-group-item";
+
+  //humidity
+  var humidity = document.createElement("span");
+  humidity.textContent = "Humidity:" + weather.main.humidity + "%";
+
+  //windspeed
+  var windSpeed = document.createElement("span");
+  windSpeed.textContent = "Windspeed:" + weather.wind.speed + "mph";
+
+  //uvindex
+
+  // var uv = document.createElement("span");
+  // uv.textContent = "UV Index: " + weather.uv.index;
+
+  //appends so that pieces will show in container
+  weatherContainerEl.appendChild(temperature);
+
+  weatherContainerEl.appendChild(humidity);
+
+  weatherContainerEl.appendChild(windSpeed);
+};
+
+var getUv = function (lat, lon) {
+  var apiKey = "9fc0d146e8b9c3492bad6e84401335c1";
+  var apiWeather = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+
+  fetch(apiWeather).then(function (response) {
+    response.json().then(function (data) {
+      displayUv(data);
+    });
+  });
+};
+
+var displayUv = function (uvShow) {
+  var uv = document.createElement("div");
+  uv.textContent = "UV Index: ";
+  uv.textContent.classList = "list-group-item";
+
+  uvValue = document.createElement("span");
+  uvValue.textContent = uvShow.value;
+
+  if (uvShow.value < 3) {
+    uvValue.classList = "favorable";
+  } else if (uvShow.value > 3 && uvShow.value < 5) {
+    uvValue.classList = "moderate";
+  } else if (uvShow.value > 5) {
+    uvValue.classList = "severe";
+
+    uv.appendChild(uvValue);
+
+    weatherContainerEl.appendChild(uv);
   }
 };
 
 userFormEl.addEventListener("submit", citySearch);
+cityNameEl.addEventListener("click", displayWeather);
